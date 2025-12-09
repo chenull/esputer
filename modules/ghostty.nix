752b7cc132d4c2506c5e9bae9a5e49a84995f4eb
Sayid Munawar <ayik@jogjacamp.co.id>
2025-12-09 22:24:59 +0700

    styling

--- modules/ghostty.nix
+++ modules/ghostty.nix
@@ -15,10 +15,13 @@
 
     programs.ghostty.settings = {
       theme = "Starlight";
+      font-size = 12;
       bold-is-bright = true;
       quit-after-last-window-closed = true;
       auto-update = lib.mkIf pkgs.stdenv.hostPlatform.isDarwin "off";
-      keybind = "shift+enter=text:\\n";
+      keybind = [
+        "shift+enter=text:\\n"
+      ];
     };
   };
 }
